bd52895f4fc6bdf9f9b2a941ee258af335de0228
Bhuban Ghimire <bhuban@nsdevil.com>
2026-06-26 11:41:04 +0545

    model update of qr

--- Qr/models.py
+++ Qr/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from pytz import timezone
 
 from accounts.views import User
 from system.models import ConfigChoice, SoftDeletable
@@ -9,8 +10,77 @@ class Project(SoftDeletable):
     owner = models.ForeignKey(User, on_delete=models.RESTRICT)
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    # industry = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT)
     status = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT)
 
     def __str__(self):
         return self.name
+    
+
+class QRCode(SoftDeletable):
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.RESTRICT)
+    name = models.CharField(max_length=200)
+    qr_type = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT)
+    status = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT)
+
+
+class QRCodeData(SoftDeletable):
+    qr_code = models.ForeignKey(QRCode, on_delete=models.CASCADE)
+    content_json = models.JSONField()
+
+
+class QRSchedule(SoftDeletable):
+    qr_code = models.ForeignKey(QRCode, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    timezone = models.CharField(max_length=100)
+    is_scheduled = models.BooleanField(default=False)
+
+
+class QRScanSetting(SoftDeletable):
+    qr_code = models.ForeignKey(QRCode, on_delete=models.CASCADE)
+    is_scan_limit = models.BooleanField(default=False)
+    scan_limit = models.PositiveIntegerField(null=True, blank=True)
+    is_time_limit = models.BooleanField(default=False)
+    time_limit = models.PositiveIntegerField(null=True, blank=True)  # in seconds
+
+
+class QRDesign(SoftDeletable):
+    qr_code = models.ForeignKey(QRCode, on_delete=models.CASCADE)
+    eye_style = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT, related_name='eye_style')
+    pattern_style = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT, related_name='pattern_style')
+    foreground_color = models.CharField(max_length=7)  # Hex color code
+    background_color = models.CharField(max_length=7)  # Hex color code
+    logo = models.ImageField(upload_to='qr_logos/', null=True, blank=True)
+    frame = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "QR Designs"
+
+    def __str__(self):
+        return f"Design for QR Code ID: {self.qr_code.id}"
+
+
+
+
+class invitations(SoftDeletable):
+    email = models.EmailField()
+    resource_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    resource_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'resource_id')
+
+    role = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT)
+    token = models.CharField(max_length=100, unique=True)
+    invited_by = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='invitations_sent')
+    status = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT)
+    accepted_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class SharePermissions(SoftDeletable):
+    user_id = models.ForeignKey(User, on_delete=models.RESTRICT)
+    resource_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    resource_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'resource_id')
+    role = models.ForeignKey(ConfigChoice, on_delete=models.RESTRICT)
